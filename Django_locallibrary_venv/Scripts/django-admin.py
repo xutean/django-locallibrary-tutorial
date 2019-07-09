151a61ad1dd8f16eb85f09befe5595e85cf354ab
git.xutean <git.xutean@gmail.com>
2019-07-09 18:58:44 +0800

    add django-locallibrary-venv

---
+++ Django_locallibrary_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\xutean\django-locallibrary-tutorial\django_locallibrary_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
